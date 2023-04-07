2e5de7a26f67e5555b0ebfde984747d8f4719761
Dani John <rocksdanister@gmail.com>
2023-04-07 12:24:15 +0530

    refactoring and scroll fn

--- dani/js/index.js
+++ dani/js/index.js
@@ -1,3 +1,4 @@
+//app ui
 var ul = document.getElementById("wallpapers");
 ul.onclick = function (event) {
   let lis = document.getElementById("wallpapers").getElementsByTagName("IMG");
@@ -26,6 +27,7 @@ ul.onclick = function (event) {
   }
 };
 
+//app ui (scrolling)
 $(window).scroll(function () {
   var requiredOffset = 100;
 
@@ -55,24 +57,12 @@ $(window).scroll(function () {
 
 });
 
-const rangeInputs = document.querySelectorAll('input[type="range"]');
-
-function handleInputChange(e) {
-  let target = e.target;
-  if (e.target.type !== "range") {
-    target = document.getElementById("range");
-  }
-  const min = target.min;
-  const max = target.max;
-  const val = target.value;
-
-  target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
-}
-
-rangeInputs.forEach((input) => {
-  input.addEventListener("input", handleInputChange);
-});
-
+//helpers
 function hasClass(element, className) {
   return (" " + element.className + " ").indexOf(" " + className + " ") > -1;
+}
+
+function scrollToElement(id)
+{
+  document.getElementById(id).scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
