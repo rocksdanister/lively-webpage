9a9ea4c265dd800c3cf1191b68727c6b77c89f5f
Dani John <rocksdanister@gmail.com>
2023-04-11 14:24:28 +0530

    loading indicator
    resolved: https://github.com/rocksdanister/lively-webpage/issues/25

--- js/index.js
+++ js/index.js
@@ -88,7 +88,20 @@ $(window).on("scroll", function () {
   }
 });
 
+//threejs scene first run
+document.addEventListener("sceneLoaded", () => {
+  if (container.style.opacity == 0) setVisible(container);
+  $(".indeterminate-progress-bar").css("display", "none");
+});
+
 //helpers
+async function setVisible(element) {
+  for (let val = 0; val < 1; val += 0.1) {
+    element.style.opacity = val;
+    await new Promise((r) => setTimeout(r, 75));
+  }
+}
+
 function hasClass(element, className) {
   return (" " + element.className + " ").indexOf(" " + className + " ") > -1;
 }
